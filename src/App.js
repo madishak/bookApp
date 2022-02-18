d2b40b5236bf21c694401a4bfba60caff04a7d7f
Madina Karachai <medinakarachai@mail.ru>
2022-02-18 11:51:07 +0300

    Modified file

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import React from "react";
-// import { Switch, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import BookForm from "./containers/BookForm/BookForm";
+import BookCards from "./containers/BookCards/BookCards";
+import BookCardView from "./containers/BookCardView/BookCardView";
+import NotFound from "./components/NotFound/NotFound";
 
 const App = () => {
-
   return (
-      <div>Obi-Wan Fluffy
-          <BookForm/>
+    <BrowserRouter>
+      <div>
+        <BookForm />
+        <Routes>
+          <Route path="/" element={<BookCards />} />
+          <Route path="/:id" element={<BookCardView />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
       </div>
-    // <Switch>
-    //   <Route exact path="/" component={Home} />
-    //   <Route exact path="/:id" component={PhotoCardView} />
-    // </Switch>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
--- src/rootReducer.js
+++ src/rootReducer.js
@@ -1,7 +1,9 @@
 import { combineReducers } from "redux";
 import books from "./containers/BookForm/reducer";
+import bookID from "./containers/BookCards/reducer";
 
 
 export default combineReducers({
     books,
+    bookID,
 });
