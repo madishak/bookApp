970b41d648748fad879e5655aad3d673165e3655
Madina Karachai <medinakarachai@mail.ru>
2022-02-18 23:48:28 +0300

    Added react dom router v6

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import BookForm from "./containers/BookForm/BookForm";
 import BookCards from "./containers/BookCards/BookCards";
 import BookCardView from "./containers/BookCardView/BookCardView";
-import NotFound from "./components/NotFound/NotFound";
 
 const App = () => {
   return (
@@ -13,7 +12,6 @@ const App = () => {
         <Routes>
           <Route path="/" element={<BookCards />} />
           <Route path="/:id" element={<BookCardView />} />
-          <Route path="*" element={<NotFound />} />
         </Routes>
       </div>
     </BrowserRouter>
